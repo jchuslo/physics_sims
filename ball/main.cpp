92291a9a64c6b5328f69fe68a782ae948419bc58
chusloj <pyrotekk66@gmail.com>
2021-02-04 22:35:10 -0500

    completed basic ball bouncing simulation

--- ball/main.cpp
+++ ball/main.cpp
@@ -8,14 +8,12 @@ int main()
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Red);
 
-    std::clock_t start;
-    start = std::clock();
-
     float move_x = 0.2;
     float move_y = 0.8;
-    const float move_y_with_gravity = 1;
-    const float move_y_without_gravity = 1;
+    const float move_y_with_gravity = 1.005;
+    const float move_y_without_gravity = 0.995;
     bool direct_down = true;
+    bool direct_right = true;
 
     while (window.isOpen())
     {
@@ -28,26 +26,36 @@ int main()
 
         window.clear();
 
-        if (std::abs(shape.getPosition().y - 1000) < 10) {
+        if (shape.getPosition().x > 1000 && direct_right == true) {
+            // TODO: make this general so that it uses the length of screen as parameter
+        	move_x *= -1;
+        	direct_right = false;
+        }
+
+        if (shape.getPosition().x < 0 && direct_right == false) {
+            // TODO: make this general so that it uses the length of screen as parameter
+        	move_x *= -1;
+        	direct_right = true;
+        }
+
+        if (shape.getPosition().y > 1000 && direct_down == true) {
             // TODO: make this general so that it uses the length of screen as parameter
         	move_y *= -1;
         	direct_down = false;
         }
 
-        if (std::abs(shape.getPosition().y - 0) < 5 && direct_down == false) {
+        if (shape.getPosition().y < 0 && direct_down == false) {
         	move_y *= -1;
         	direct_down = true;
         }
         
-        if ((clock() - start) % 2 == 0) {
-        	shape.move(move_x, move_y);
-        }
+    	shape.move(move_x, move_y);
 
         window.draw(shape);
         window.display();
         
         direct_down == true ? move_y *= move_y_with_gravity : move_y *= move_y_without_gravity;
-        if((abs(move_y) < 0.0000000001) && direct_down == false) {
+        if((abs(move_y) < 0.5) && direct_down == false) {
             direct_down = true;
             move_y = 0.8;
         }
